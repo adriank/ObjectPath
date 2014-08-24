5394ace75e769bb8423065c2df6a0833d78259b6
Adrian Kalbarczyk <adrian.kalbarczyk@gmail.com>
2014-08-24 02:31:28 +0200

    added colors:)

---
+++ ObjectPathPy/utils/colorify.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def color(c,s):
+	return u'\033[%sm%s\033[0m' % (c,s)
+
+def bold(s):
+	return color(1,s)
+
+def op(s):
+	return color(32,bold(s))
+
+def const(s):
+	return color(36,bold(s))
+
+def string(s):
+	return color(33,bold(s))
+
