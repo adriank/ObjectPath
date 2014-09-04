6cc4c81d5a50f65db188c13a92b86e01a7b3387c
Adrian Kalbarczyk <adrian.kalbarczyk@gmail.com>
2014-09-04 03:16:17 +0200

    PyPI installation now includes interactive shell

---
+++ ObjectPathPy/objectpath.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from objectpath import shell
+
+shell.main()
--- ObjectPathPy/setup.py
+++ ObjectPathPy/setup.py
@@ -7,20 +7,22 @@ pandoc.core.PANDOC_PATH = '/usr/bin/pandoc'
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-def re2md(s):
+def md2re(s):
 	doc = pandoc.Document()
 	doc.markdown = s
 	return doc.rst
 
 long_description = (
-    re2md(read('README.md'))
+    md2re(read('../README.md'))
     + '\n' +
     'Download\n'
     '********\n'
     )
 
+print long_description
+
 setup(name='objectpath',
-			version='0.2',
+			version='0.3',
 			description='The agile query language for semi-structured data. #JSON',
 			long_description=long_description,
 			url='http://adriank.github.io/ObjectPath',
@@ -30,11 +32,17 @@ setup(name='objectpath',
 			packages=['objectpath','objectpath.utils','objectpath.core'],
 			keywords="query, tree, JSON, nested structures",
 			classifiers=[
-				"Development Status :: 6 - Mature"
+				"Development Status :: 6 - Mature",
 				"Intended Audience :: Developers",
 				"Intended Audience :: Science/Research",
 				"License :: OSI Approved :: GNU Affero General Public License v3",
 				"Programming Language :: Python",
 				"Topic :: Software Development :: Libraries :: Python Modules"
 			],
-			zip_safe=True)
+			zip_safe=True,
+			entry_points = {
+				'console_scripts': [
+					'objectpath = objectpath.shell:main'
+				]
+			}
+		)
