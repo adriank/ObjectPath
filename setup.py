8e6ea98a8af383360a34c921131d0ecbb1c19dde
Erik Bernoth <erik.bernoth@gmail.com>
2015-07-03 11:49:48 +0200

    pythonic roformatting of setup.py
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>

--- setup.py
+++ setup.py
@@ -2,39 +2,40 @@ import os
 from setuptools import setup
 
 def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    filename = os.path.join(os_path.dirname(__file__), *rnames)
+    with open(filename) as f:
+        return f.read()
 
-long_description = (
-    read('README.rst')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
+long_description = """{}
 
-setup(name='objectpath',
-			version=read('VER').strip(),
-			description='The agile query language for semi-structured data. #JSON',
-			long_description=long_description,
-			url='http://adriank.github.io/ObjectPath',
-			author='Adrian Kalbarczyk',
-			author_email='adrian.kalbarczyk@gmail.com',
-			license='AGPLv3',
-			packages=['objectpath','objectpath.utils','objectpath.core'],
-			# package_dir={'': 'objectpath'},
-			keywords="query, tree, JSON, nested structures",
-			classifiers=[
-				"Development Status :: 6 - Mature",
-				"Intended Audience :: Developers",
-				"Intended Audience :: Science/Research",
-				"License :: OSI Approved :: GNU Affero General Public License v3",
-				"Programming Language :: Python",
-				"Topic :: Software Development :: Libraries :: Python Modules"
-			],
-			zip_safe=True,
-			entry_points = {
-				'console_scripts': [
-					'objectpath = objectpath.shell:main'
-				]
-			},
-			test_suite="tests"
-		)
+Download
+********""".format(read('README.rst'))
+
+setup(
+    name='objectpath',
+    version=read('VER').strip(),
+    description='The agile query language for semi-structured data. #JSON',
+    long_description=long_description,
+    url='http://adriank.github.io/ObjectPath',
+    author='Adrian Kalbarczyk',
+    author_email='adrian.kalbarczyk@gmail.com',
+    license='AGPLv3',
+    packages=['objectpath','objectpath.utils','objectpath.core'],
+    # package_dir={'': 'objectpath'},
+    keywords="query, tree, JSON, nested structures",
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    zip_safe=True,
+    entry_points = {
+        'console_scripts': [
+            'objectpath = objectpath.shell:main'
+        ]
+    },
+    test_suite="tests",
+)
