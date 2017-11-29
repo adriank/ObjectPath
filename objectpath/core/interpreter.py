6b35f0589a970d2c125526d26bcac871b49fdec9
Andrey Alexandrov <andrey.aleksandrov@gmail.com>
2017-11-29 12:28:39 +0300

    PEP 8

--- objectpath/core/interpreter.py
+++ objectpath/core/interpreter.py
@@ -3,7 +3,8 @@
 # This file is part of ObjectPath released under AGPL v3 license.
 # Copyright (C) 2010-2014 Adrian Kalbarczyk
 
-import sys, re
+import sys
+import re
 from .parser import parse
 from objectpath.core import *
 import objectpath.utils.colorify as color  # pylint: disable=W0614
@@ -67,7 +68,8 @@ class Tree(Debugger):
 
     def execute(self, expr):
         D = self.D
-        if D: self.start("Tree.execute")
+        if D:
+            self.start("Tree.execute")
         TYPES = [str, int, float, bool, generator, chain]
         try:
             TYPES += [long]
@@ -85,7 +87,8 @@ class Tree(Debugger):
                 types += [unicode]
             except:
                 pass
-            if D: self.start("executing node '%s'", node)
+            if D:
+                self.start("executing node '%s'", node)
             type_node = type(node)
             if node is None or type_node in TYPES:
                 return node
@@ -100,10 +103,12 @@ class Tree(Debugger):
                 return ret
             op = node[0]
             if op == "or":
-                if D: self.debug("%s or %s", node[1], node[2])
+                if D:
+                    self.debug("%s or %s", node[1], node[2])
                 return exe(node[1]) or exe(node[2])
             elif op == "and":
-                if D: self.debug("%s and %s", node[1], node[2])
+                if D:
+                    self.debug("%s and %s", node[1], node[2])
                 return exe(node[1]) and exe(node[2])
             elif op == "+":
                 if len(node) > 2:
@@ -123,14 +128,16 @@ class Tree(Debugger):
                                 color.bold("object")))
                         return fst
                     if typefst is list and typesnd is list:
-                        if D: self.debug("both sides are lists, returning '%s'", fst + snd)
+                        if D:
+                            self.debug("both sides are lists, returning '%s'", fst + snd)
                         return fst + snd
                     if typefst in ITER_TYPES or typesnd in ITER_TYPES:
                         if typefst not in ITER_TYPES:
                             fst = [fst]
                         elif typesnd not in ITER_TYPES:
                             snd = [snd]
-                        if D: self.debug("at least one side is generator and other is iterable, returning chain")
+                        if D:
+                            self.debug("at least one side is generator and other is iterable, returning chain")
                         return chain(fst, snd)
                     if typefst in NUM_TYPES:
                         try:
@@ -138,7 +145,8 @@ class Tree(Debugger):
                         except Exception:
                             return fst + float(snd)
                     if typefst in STR_TYPES or typesnd in STR_TYPES:
-                        if D: self.info("doing string comparison '%s' is '%s'", fst, snd)
+                        if D:
+                            self.info("doing string comparison '%s' is '%s'", fst, snd)
                         if sys.version_info[0] < 3:
                             if typefst is unicode:
                                 fst = fst.encode("utf-8")
@@ -151,7 +159,8 @@ class Tree(Debugger):
                             return timeutils.addTimes(fst, snd)
                     except Exception:
                         pass
-                    if D: self.debug("standard addition, returning '%s'", fst + snd)
+                    if D:
+                        self.debug("standard addition, returning '%s'", fst + snd)
                     return fst + snd
                 else:
                     return exe(node[1])
@@ -176,7 +185,8 @@ class Tree(Debugger):
             elif op == "/":
                 return exe(node[1]) / float(exe(node[2]))
             elif op == ">":
-                if D: self.debug("%s > %s, %s", node[1], node[2], node[1] > node[2])
+                if D:
+                    self.debug("%s > %s, %s", node[1], node[2], node[1] > node[2])
                 return exe(node[1]) > exe(node[2])
             elif op == "<":
                 return exe(node[1]) < exe(node[2])
@@ -198,7 +208,8 @@ class Tree(Debugger):
             #         return fst <= snd and snd or False
             elif op == "not":
                 fst = exe(node[1])
-                if D: self.debug("doing not '%s'", fst)
+                if D:
+                    self.debug("doing not '%s'", fst)
                 return not fst
             elif op == "in":
                 fst = exe(node[1])
@@ -210,12 +221,14 @@ class Tree(Debugger):
             elif op == "not in":
                 fst = exe(node[1])
                 snd = exe(node[2])
-                if D: self.debug("doing '%s' not in '%s'", node[1], node[2])
+                if D:
+                    self.debug("doing '%s' not in '%s'", node[1], node[2])
                 if type(fst) in ITER_TYPES and type(snd) in ITER_TYPES:
                     return not any(x in max(fst, snd, key=len) for x in min(fst, snd, key=len))
                 return exe(node[1]) not in exe(node[2])
             elif op in ("is", "is not"):
-                if D: self.debug("found operator '%s'", op)
+                if D:
+                    self.debug("found operator '%s'", op)
                 # try:
                 fst = exe(node[1])
                 # except Exception as e:
@@ -233,21 +246,27 @@ class Tree(Debugger):
                 #     return True
                 typefst = type(fst)
                 typesnd = type(snd)
-                if D: self.debug("type fst: '%s', type snd: '%s'", typefst, typesnd)
+                if D:
+                    self.debug("type fst: '%s', type snd: '%s'", typefst, typesnd)
                 if typefst in STR_TYPES:
-                    if D: self.info("doing string comparison '\"%s\" is \"%s\"'", fst, snd)
+                    if D:
+                        self.info("doing string comparison '\"%s\" is \"%s\"'", fst, snd)
                     ret = fst == str(snd)
                 elif typefst is float:
-                    if D: self.info("doing float comparison '%s is %s'", fst, snd)
+                    if D:
+                        self.info("doing float comparison '%s is %s'", fst, snd)
                     ret = abs(fst - float(snd)) < EPSILON
                 elif typefst is int:
-                    if D: self.info("doing integer comparison '%s is %s'", fst, snd)
+                    if D:
+                        self.info("doing integer comparison '%s is %s'", fst, snd)
                     ret = fst == int(snd)
                 elif typefst is list and typesnd is list:
-                    if D: self.info("doing array comparison '%s' is '%s'", fst, snd)
+                    if D:
+                        self.info("doing array comparison '%s' is '%s'", fst, snd)
                     ret = fst == snd
                 elif typefst is dict and typesnd is dict:
-                    if D: self.info("doing object comparison '%s' is '%s'", fst, snd)
+                    if D:
+                        self.info("doing object comparison '%s' is '%s'", fst, snd)
                     ret = fst == snd
                 # else:
                 #     try:
@@ -263,10 +282,12 @@ class Tree(Debugger):
                 #     except Exception:
                 #         pass
                 if op == "is not":
-                    if D: self.info("'is not' found. Returning %s", not ret)
+                    if D:
+                        self.info("'is not' found. Returning %s", not ret)
                     return not ret
                 else:
-                    if D: self.info("returning '%s' is '%s'='%s'", fst, snd, ret)
+                    if D:
+                        self.info("returning '%s' is '%s'='%s'", fst, snd, ret)
                     return ret
             elif op == "re":
                 return re.compile(exe(node[1]))
@@ -284,7 +305,8 @@ class Tree(Debugger):
             #     if D: self.debug("returning node %s",self.node)
             #     return self.node
             elif op == "(current)":  # this is @
-                if D: self.debug("returning current node %s", self.current)
+                if D:
+                    self.debug("returning current node %s", self.current)
                 return self.current
             elif op == "name":
                 return node[1]
@@ -293,55 +315,67 @@ class Tree(Debugger):
                 if type(fst) is tuple:
                     fst = exe(fst)
                 typefst = type(fst)
-                if D: self.debug(color.op(".") + " left is '%s'", fst)
+                if D:
+                    self.debug(color.op(".") + " left is '%s'", fst)
                 # try:
                 if node[2][0] == "*":
-                    if D: self.end(color.op(".") + " returning '%s'", typefst in ITER_TYPES and fst or [fst])
+                    if D:
+                        self.end(color.op(".") + " returning '%s'", typefst in ITER_TYPES and fst or [fst])
                     return fst  # typefst in ITER_TYPES and fst or [fst]
                 # except:
                 #     pass
                 snd = exe(node[2])
-                if D: self.debug(color.op(".") + " right is '%s'", snd)
+                if D:
+                    self.debug(color.op(".") + " right is '%s'", snd)
                 if typefst in ITER_TYPES:
-                    if D: self.debug(color.op(".") + " filtering %s by %s", color.bold(fst), color.bold(snd))
+                    if D:
+                        self.debug(color.op(".") + " filtering %s by %s", color.bold(fst), color.bold(snd))
                     if type(snd) in ITER_TYPES:
                         return filter_dict(fst, list(snd))
                     else:
                         # if D: self.debug(list(fst))
                         return (e[snd] for e in fst if type(e) is dict and snd in e)
                 try:
-                    if D: self.end(color.op(".") + " returning '%s'", fst.get(snd))
+                    if D:
+                        self.end(color.op(".") + " returning '%s'", fst.get(snd))
                     return fst.get(snd)
                 except Exception:
                     if isinstance(fst, object):
                         return self.object_getter(fst, snd)
-                    if D: self.end(color.op(".") + " returning '%s'", color.bold(fst))
+                    if D:
+                        self.end(color.op(".") + " returning '%s'", color.bold(fst))
                     return fst
             elif op == "..":
                 fst = flatten(exe(node[1]))
                 if node[2][0] == "*":
-                    if D: self.debug(color.op("..") + " returning '%s'", color.bold(fst))
+                    if D:
+                        self.debug(color.op("..") + " returning '%s'", color.bold(fst))
                     return fst
                 # reduce objects to selected attributes
                 snd = exe(node[2])
-                if D: self.debug(color.op("..") + " finding all %s in %s", color.bold(snd), color.bold(fst))
+                if D:
+                    self.debug(color.op("..") + " finding all %s in %s", color.bold(snd), color.bold(fst))
                 if type(snd) in ITER_TYPES:
                     ret = filter_dict(fst, list(snd))
-                    if D: self.debug(color.op("..") + " returning %s", color.bold(ret))
+                    if D:
+                        self.debug(color.op("..") + " returning %s", color.bold(ret))
                     return ret
                 else:
                     ret = chain(*(type(x) in ITER_TYPES and x or [x] for x in (e[snd] for e in fst if snd in e)))
                     # print list(chain(*(type(x) in ITER_TYPES and x or [x] for x in (e[snd] for e in fst if snd in e))))
-                    if D: self.debug(color.op("..") + " returning %s", color.bold(ret))
+                    if D:
+                        self.debug(color.op("..") + " returning %s", color.bold(ret))
                     return ret
             elif op == "[":
                 len_node = len(node)
                 # TODO move it to tree generation phase
                 if len_node is 1:  # empty list
-                    if D: self.debug("returning an empty list")
+                    if D:
+                        self.debug("returning an empty list")
                     return []
                 if len_node is 2:  # list - preserved to catch possible event of leaving it as '[' operator
-                    if D: self.debug("doing list mapping")
+                    if D:
+                        self.debug("doing list mapping")
                     return [exe(x) for x in node[1]]
                 if len_node is 3:  # selector used []
                     fst = exe(node[1])
@@ -349,25 +383,30 @@ class Tree(Debugger):
                     if not fst:
                         return fst
                     selector = node[2]
-                    if D: self.debug("found '%s' selector. executing on %s", color.bold(selector), color.bold(fst))
+                    if D:
+                        self.debug("found '%s' selector. executing on %s", color.bold(selector), color.bold(fst))
                     selectorIsTuple = type(selector) is tuple
 
                     if selectorIsTuple and selector[0] is "[":
                         nodeList = []
                         nodeList_append = nodeList.append
                         for i in fst:
-                            if D: self.debug("setting self.current to %s", color.bold(i))
+                            if D:
+                                self.debug("setting self.current to %s", color.bold(i))
                             self.current = i
                             nodeList_append(exe((selector[0], exe(selector[1]), exe(selector[2]))))
-                        if D: self.debug("returning %s objects: %s", color.bold(len(nodeList)), color.bold(nodeList))
+                        if D:
+                            self.debug("returning %s objects: %s", color.bold(len(nodeList)), color.bold(nodeList))
                         return nodeList
 
                     if selectorIsTuple and selector[0] == "(current)":
-                        if D: self.warning(color.bold("$.*[@]") + " is eqivalent to " + color.bold("$.*") + "!")
+                        if D:
+                            self.warning(color.bold("$.*[@]") + " is eqivalent to " + color.bold("$.*") + "!")
                         return fst
 
                     if selectorIsTuple and selector[0] in SELECTOR_OPS:
-                        if D: self.debug("found %s operator in selector, %s", color.bold(selector[0]),
+                        if D:
+                            self.debug("found %s operator in selector, %s", color.bold(selector[0]),
                                          color.bold(selector))
                         if type(fst) is dict:
                             fst = [fst]
@@ -400,10 +439,13 @@ class Tree(Debugger):
                                         # TODO optimize an event when @ is not used. exe(selector1) can be cached
                                         if exe((selector0, exe(selector1), exe(selector2))):
                                             yield i
-                                            if D: self.debug("appended")
-                                        if D: self.debug("discarded")
+                                            if D:
+                                                self.debug("appended")
+                                        if D:
+                                            self.debug("discarded")
                                     except Exception:
-                                        if D: self.debug("discarded")
+                                        if D:
+                                            self.debug("discarded")
 
                         # if D and nodeList: self.debug("returning '%s' objects: '%s'", color.bold(len(nodeList)), color.bold(nodeList))
                         return exeSelector(fst)
@@ -415,7 +457,8 @@ class Tree(Debugger):
                         # nodes[N]
                         if typesnd in NUM_TYPES or typesnd is str and snd.isdigit():
                             n = int(snd)
-                            if D: self.info("getting %sth element from '%s'", color.bold(n), color.bold(fst))
+                            if D:
+                                self.info("getting %sth element from '%s'", color.bold(n), color.bold(fst))
                             if typefst in (generator, chain):
                                 if n > 0:
                                     return skip(fst, n)
@@ -436,11 +479,13 @@ class Tree(Debugger):
                             return snd
                     else:
                         try:
-                            if D: self.debug("returning %s", color.bold(fst[snd]))
+                            if D:
+                                self.debug("returning %s", color.bold(fst[snd]))
                             return fst[snd]
                         except KeyError:
                             # CHECK - is it ok to do that or should it be ProgrammingError?
-                            if D: self.debug("returning an empty list")
+                            if D:
+                                self.debug("returning an empty list")
                             return []
                 raise ProgrammingError("Wrong usage of " + color.bold("[") + " operator")
             elif op == "fn":
@@ -450,7 +495,8 @@ class Tree(Debugger):
                 try:
                     args = [exe(x) for x in node[2:]]
                 except IndexError:
-                    if D: self.debug("NOT ERROR: can't map '%s' with '%s'", node[2:], exe)
+                    if D:
+                        self.debug("NOT ERROR: can't map '%s' with '%s'", node[2:], exe)
                 # arithmetic
                 if fnName == "sum":
                     args = args[0]
@@ -522,7 +568,8 @@ class Tree(Debugger):
                     try:
                         pos = list(args[1])
                         if type(pos[0]) in ITER_TYPES:
-                            if D: self.debug("run slice() for a list of slicers")
+                            if D:
+                                self.debug("run slice() for a list of slicers")
                             return (args[0][x[0]:x[1]] for x in pos)
                         return args[0][pos[0]:pos[1]]
                     except IndexError:
@@ -554,7 +601,8 @@ class Tree(Debugger):
                     else:
                         a = {}
                     args = args[0]
-                    if D: self.debug("doing sort on '%s'", args)
+                    if D:
+                        self.debug("doing sort on '%s'", args)
                     try:
                         return sorted(args, **a)
                     except TypeError:
@@ -652,7 +700,8 @@ class Tree(Debugger):
         elif type(expr) not in (tuple, list, dict):
             return expr
         ret = exe(tree)
-        if D: self.end("Tree.execute with: '%s'", ret)
+        if D:
+            self.end("Tree.execute with: '%s'", ret)
         return ret
 
     def __str__(self):
